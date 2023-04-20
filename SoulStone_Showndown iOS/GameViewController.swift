6bca73ac7370035069ed991cbacae4b6401186c6
Calvin Teater <laughingglint3@gmail.com>
2023-04-20 15:48:12 -0500

    Initial Commit

---
+++ SoulStone_Showndown iOS/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  SoulStone_Showdown iOS
+//
+//  Created by Calvin Grant Teater on 4/13/23.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+    
+    //global variables
+    var scene: SCNScene!
+    var sceneView: SCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildGame()
+    }
+    
+    func buildGame() {
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
